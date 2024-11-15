9e4ce137729b96bc2066fc3e5dacd92ef9427ab3
Antonio Drumond <adcdstom@gmail.com>
2024-11-15 14:05:17 -0300

    Ate 1505

--- Guias/ED15/Guia_1503.asm
+++ Guias/ED15/Guia_1503.asm
@@ -23,33 +23,13 @@ MAIN:	CALL LOADER ; Carrega dados iniciais na memoria
 	LDA C0E0
 	LXI H,C0F0
 	SUB M
-	STA C0E2
-	
+
 	LXI H,C100
-	MOV B,M
-	                     ;  Altera o fluxo de execucao para caso o resultado 
-	JC NEG ;      da subtracao seja negativo
+	ADD M
 
-	ADD B     ; Caso positivo
-	JMP AFTER
-
-	; Caso negativo
-	NEG: CMA
-	INR A
-	ADD B
-	CMA
-	INR A
-
-	AFTER: STA C0E2
-
-	; Ultima subtracao
 	LXI H,C110
 	SUB M
-	
-	JNC AFTER2
-	CMA
-	INR A
 
-	AFTER2: STA C0E2
+	STA C0E2
 	
 HLT
